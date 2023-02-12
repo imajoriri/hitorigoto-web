81ec81dfecd7f7e61deb3b796b0bc6df622b67e4
imajo <imajo.srroni@gmail.com>
2023-02-12 22:53:01 +0900

    docs

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  outputDir: "docs",
+  assetsDir: "./",
+  publicPath: "./",
 
   pluginOptions: {
     vuetify: {
